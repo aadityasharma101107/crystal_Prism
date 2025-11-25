9fbcea36846910c76d97dad5c2ee3dcfdd216885
Aaditya Sharma <aadityasharma101107@gmail.com>
2025-11-25 15:10:04 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+ument.addEventListener("mousemove", e=> {
+    const prism = document.getElementById("prism");
+    let x = (window.innerWidth/ 2 -e.clientX) / 40;
+    let y = (window.innerHeight / 2 - e.clientY) / 40;
+    prism.style.transform = `rotateX(${20 - y}deg) rotateY(${20 - x}deg)`;
+});
